0583eb0c20fc6c477c04d8dc5b19e6babca9ecaa
koena <koena@LAPTOP-NTVQFOBG.home>
2020-08-28 11:39:53 +0200

    Begin nieuwe suite

---
+++ Test Suites/Rollen en rechten/Staff.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Staff</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3dceff82-072f-4701-a9bd-422a6b319d84</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0731d1b-a24e-4ba7-aaca-f92830861b9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rollen en Rechten/1. Menu en Profiel/1.1a Clubmenu zien</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9fa1afd-2daf-457d-990d-da986e1211af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rollen en Rechten/2. Ledenoverzicht/2.1a. Alle leden in ledenoverzicht zien</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
